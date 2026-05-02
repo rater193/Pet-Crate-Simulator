eb0d4353bce83b7356e12c7c11d71e673ce32a79
rater <rater193@gmail.com>
2026-05-02 02:35:18 -0400

    Added the displayname to the pet components

--- Code/Pets/PetComponent.cs
+++ Code/Pets/PetComponent.cs
@@ -2,6 +2,7 @@ using Sandbox;
 
 public sealed class PetComponent : Component
 {
+	[Property] public string DisplayName { get; set; } = "Pet";
 	[Property] public float CoinMultiplier { get; set; } = 1f;
 	[Property] public int Damage { get; set; } = 1;
 	[Property] public float AttackInterval { get; set; } = 1f;
