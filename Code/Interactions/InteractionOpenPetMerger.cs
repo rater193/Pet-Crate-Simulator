7ad940a9d6c61f33369f8bb553e66da5739e163e
rater <rater193@gmail.com>
2026-05-09 03:01:32 -0400

    .

---
+++ Code/Interactions/InteractionOpenPetMerger.cs
@@ -0,0 +1,12 @@
+using Sandbox;
+
+public sealed class InteractionOpenPetMerger : Interactable
+{
+	public override void OnInteract( PlayerController interactingPlayer )
+	{
+		if(!interactingPlayer.IsProxy)
+		{
+
+		}
+	}
+}
